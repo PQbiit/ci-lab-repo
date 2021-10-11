ce45c7b8fa75d54b03175e3bdc955facebf2801e
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-10-11 14:21:24 -0500

    Implemented test for add function

--- project.js
+++ project.js
@@ -1,5 +1,6 @@
-function getGreeting(name){
-    return "Hi " + name;
+module.exports = {
+    getGreeting: (name) =>{
+        return "Hi " + name;
+    },
+    add: (n1,n2) => parseInt(n1) + parseInt(n2)
 }
-
-module.exports = getGreeting;
--- test.js
+++ test.js
@@ -1,5 +1,16 @@
-const getGreeting = require('./project');
+const {getGreeting, add} = require('./project');
 
-test('can create a basic greeting', () => {
-  expect(getGreeting('Sam')).toBe("Hi Sam");
+describe('getGreeting function tests', ()=>{
+  test('can create a basic greeting', () => {
+    expect(getGreeting('Sam')).toBe("Hi Sam");
+  });
+});
+
+describe('add function tests', () =>{
+  test('adds 2 numbers (4+6 = 10)', ()=>{
+    expect(add(4,6)).toEqual(10);
+  });
+  test('If string is provided, will parse into Int',()=>{
+    expect(add('4',6)).toEqual(10);
+  });
 });
